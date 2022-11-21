df62f2361b3761739c8448fd3a648a9349cb2246
yafet-123 <yafetaddisu123@gmail.com>
2022-11-21 13:33:40 -0800

    first commit

---
+++ script.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function main(){
+
+}
+
+// we will qrite our prisma code in the main then it will catch the error message
+// in the catch and display or console the error and if it is over it will disconnect
+// at finally we can remove it because prisma disconnect as soon as we finish.
+
+main().catch(e=>{
+	console.log(e.message)
+}).finally(async ()=>{
+	await prisma.$disconnect()
+})
